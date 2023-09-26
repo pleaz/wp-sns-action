9a3ea5478c7e5f14ac1b81f839708f7f0f0fa3cf
Anton <oprstfaq@gmail.com>
2023-09-26 23:38:21 +0200

    git cache cleared

---
+++ includes/class-sns-action-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://google.com
+ * @since      1.0.0
+ *
+ * @package    Sns_Action
+ * @subpackage Sns_Action/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sns_Action
+ * @subpackage Sns_Action/includes
+ * @author     pleaz <oprstfaq@gmail.com>
+ */
+class Sns_Action_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-sns-action-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://google.com
+ * @since      1.0.0
+ *
+ * @package    Sns_Action
+ * @subpackage Sns_Action/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Sns_Action
+ * @subpackage Sns_Action/includes
+ * @author     pleaz <oprstfaq@gmail.com>
+ */
+class Sns_Action_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'sns-action',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
